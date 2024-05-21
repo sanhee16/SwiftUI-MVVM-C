d73ba0551f311f79b91f8faf88cda2698c2b578c
sanhee16 <sinhioa20@gmail.com>
2024-05-21 14:14:54 +0900

    Initial Commit

---
+++ AnimalPicker/AnimalPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalPickerApp.swift
+//  AnimalPicker
+//
+//  Created by Sandy on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimalPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
