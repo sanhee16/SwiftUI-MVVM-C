e1f6609428d7acc459060420786bcc9f58be1e16
sanhee16 <sinhioa20@gmail.com>
2024-06-19 11:18:50 +0900

    [Design] Add topbar to mainview

--- AnimalPicker/Presentation/MainView/MainView.swift
+++ AnimalPicker/Presentation/MainView/MainView.swift
@@ -28,75 +28,71 @@ struct MainView: View {
     var body: some View {
         GeometryReader { geometry in
             VStack(alignment: .leading, spacing: 0) {
-                Text("Animal Picker")
-                    .font(.kr20b)
-                    .foregroundColor(.black)
-                    .paddingBottom(12)
+                Topbar("Animal Picker")
                 
-                
-                Text("Multi Game")
-                    .font(.kr17b)
-                    .foregroundStyle(Color.black)
-                    .paddingVertical(8)
-                Text("MultiGame")
-                    .font(.kr20b)
-                    .foregroundStyle(Color.white)
-                    .paddingVertical(12)
-                    .frame(width: UIScreen.main.bounds.size.width - 40, alignment: .center)
-                    .background(
-                        RoundedRectangle(cornerRadius: 8)
-                            .foregroundStyle(Level.multi.backgroundColor)
-                    )
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        self.coordinator.pushGameRoomListView()
-                    }
-                    .shadow(color: Level.multi.backgroundColor.opacity(0.75), radius: 3, x: 2, y: 2)
-                    .paddingVertical(8)
-                    .paddingBottom(24)
-
-                Text("Single Game")
-                    .font(.kr17b)
-                    .foregroundStyle(Color.black)
-                    .paddingVertical(8)
-                
-                Text("Ranking")
-                    .font(.kr20b)
-                    .foregroundStyle(Color.black)
-                    .paddingVertical(12)
-                    .frame(width: UIScreen.main.bounds.size.width - 40, alignment: .center)
-                    .background(
-                        RoundedRectangle(cornerRadius: 8)
-                            .foregroundStyle(Color.white)
-                    )
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        self.coordinator.pushRankingView()
-                    }
-                    .clipped()
-                    .shadow(color: .black.opacity(0.25), radius: 3, x: 2, y: 2)
-                    .paddingBottom(28)
-                
-                ForEach(vm.levels, id: \.self) { level in
-                    Text(level.rawValue)
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Multi Game")
+                        .font(.kr17b)
+                        .foregroundStyle(Color.black)
+                        .paddingVertical(8)
+                    
+                    Text("MultiGame")
                         .font(.kr20b)
                         .foregroundStyle(Color.white)
                         .paddingVertical(12)
                         .frame(width: UIScreen.main.bounds.size.width - 40, alignment: .center)
                         .background(
                             RoundedRectangle(cornerRadius: 8)
-                                .foregroundStyle(level.backgroundColor)
+                                .foregroundStyle(Level.multi.backgroundColor)
                         )
                         .contentShape(Rectangle())
                         .onTapGesture {
-                            self.coordinator.pushSingleGameView(level: level)
+                            self.coordinator.pushGameRoomListView()
                         }
-                        .shadow(color: level.backgroundColor.opacity(0.75), radius: 3, x: 2, y: 2)
-                        .paddingBottom(16)
+                        .shadow(color: Level.multi.backgroundColor.opacity(0.75), radius: 3, x: 2, y: 2)
+                        .paddingBottom(24)
+                    
+                    Text("Single Game")
+                        .font(.kr17b)
+                        .foregroundStyle(Color.black)
+                        .paddingVertical(8)
+                    
+                    Text("Ranking")
+                        .font(.kr20b)
+                        .foregroundStyle(Color.black)
+                        .paddingVertical(12)
+                        .frame(width: UIScreen.main.bounds.size.width - 40, alignment: .center)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .foregroundStyle(Color.white)
+                        )
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            self.coordinator.pushRankingView()
+                        }
+                        .clipped()
+                        .shadow(color: .black.opacity(0.25), radius: 3, x: 2, y: 2)
+                        .paddingBottom(28)
+                    
+                    ForEach(vm.levels, id: \.self) { level in
+                        Text(level.rawValue)
+                            .font(.kr20b)
+                            .foregroundStyle(Color.white)
+                            .paddingVertical(12)
+                            .frame(width: UIScreen.main.bounds.size.width - 40, alignment: .center)
+                            .background(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .foregroundStyle(level.backgroundColor)
+                            )
+                            .contentShape(Rectangle())
+                            .onTapGesture {
+                                self.coordinator.pushSingleGameView(level: level)
+                            }
+                            .shadow(color: level.backgroundColor.opacity(0.75), radius: 3, x: 2, y: 2)
+                            .paddingBottom(16)
+                    }
                 }
-
-                
-                Spacer()
+                .paddingHorizontal(20)
             }
             .frame(width: geometry.size.width, alignment: .center)
         }
