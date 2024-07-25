8fef6f86e1a3252e2c530c8a1a37d2a3b852efe3
sanhee16 <sinhioa20@gmail.com>
2024-07-25 16:52:12 +0900

    Feat: Add resource
    - add bonobono
    - add rotate animation

--- AnimalPicker/Presentation/MainView/MainView.swift
+++ AnimalPicker/Presentation/MainView/MainView.swift
@@ -24,8 +24,10 @@ struct MainView: View {
     
     private var safeTop: CGFloat { get { Util.safeTop() }}
     private var safeBottom: CGFloat { get { Util.safeBottom() }}
+    private let rainbowColors: [Color] = [.red, .orange, .yellow, .green, .blue, .purple, .red]
     
     @State private var duckpos: CGRect = .zero
+    @State var rotating = 0.0
     
     var body: some View {
         GeometryReader { geometry in
@@ -74,7 +76,7 @@ struct MainView: View {
             .frame(width: geometry.size.width, alignment: .center)
         }
         .background(
-            LinearGradient(gradient: Gradient(colors: [.red, .orange, .yellow, .green, .blue, .purple, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(gradient: Gradient(colors: rainbowColors), startPoint: .topLeading, endPoint: .bottomTrailing)
         )
     }
     
@@ -84,12 +86,33 @@ struct MainView: View {
                 .resizable()
                 .scaledToFit()
                 .frame(width: 180)
-                .offset(x: 140, y: -20.0)
+                .offset(x: 150, y: -180.0)
+            
+            Image("lizard")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 210)
+                .offset(x: -80.0, y: -220.0)
+            
+            
+            Image("fox")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 230)
+                .rotationEffect(.degrees(rotating))
+                .onAppear {
+                    withAnimation(.linear(duration: 1)
+                        .speed(0.15).repeatForever(autoreverses: false)) {
+                            rotating = 360.0
+                        }
+                }
+                .offset(x: -140, y: -5.0)
+            
             Image("duck")
                 .resizable()
                 .scaledToFit()
-                .frame(width: 180)
-                .offset(x: -40.0, y: -80.0)
+                .frame(width: 200)
+                .offset(x: -40.0, y: -60.0)
                 .rectReader($duckpos, in: .global)
             
             ZStack(alignment: .top, content: {
@@ -105,16 +128,11 @@ struct MainView: View {
             })
             .offset(x: ($duckpos.wrappedValue.minX) - 60, y: -($duckpos.wrappedValue.size.height))
             
-            Image("lizard")
+            Image("bonobono")
                 .resizable()
                 .scaledToFit()
-                .frame(width: 200)
-                .offset(x: 80.0, y: 0.0)
-            Image("fox")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 230)
-                .offset(x: -120, y: -10.0)
+                .frame(width: 210)
+                .offset(x: 130, y: 5.0)
         })
         .offset(x: 0.0, y: 30.0)
     }
